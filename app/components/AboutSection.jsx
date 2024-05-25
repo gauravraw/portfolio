91201460b18b0c1f2a99db86aedb409f93fa49fb
gauravraw <40800835+gauravraw@users.noreply.github.com>
2024-05-25 14:47:32 +0530

    Update AboutSection.jsx
    Updated about section

--- app/components/AboutSection.jsx
+++ app/components/AboutSection.jsx
@@ -4,44 +4,48 @@ import Image from "next/image";
 import TabButton from "./TabButton";
 
 const TAB_DATA = [
-    {
-        title: "Skills",
-        id: "skills",
-        content: (
-            <ul className="list-disc pl-2">
-                <li>Java</li>
-                <li>Spring Boot</li>
-                <li>MySql</li>
-                <li>Data Structures</li>
-                <li>Algorithms</li>
-                <li>Kafka</li>
-                <li>WebSockets</li>
-                <li>Grpc</li>
-                <li>Redis</li>
-            </ul>
-        ) 
-    },
-    {
-        title: "Education",
-        id: "education",
-        content: (
-            <ul className="list-disc pl-2">
-                <li>B.Tech (Heritage Institute Of Technology , Kolkata (2020))<br/>
-                C.G.P.A (9.23)</li>
-            </ul>
-        ) 
-    },
-    {
-        title: "Experience",
-        id: "experience",
-        content: (
-            <ul className="list-disc pl-2">
-                <li>SDE 1 - Upstox (Current)</li>
-                <li>System Engineer - TCS</li>
-            </ul>
-        ) 
-    }
-]
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Java</li>
+        <li>Spring Boot</li>
+        <li>MySql</li>
+        <li>Data Structures</li>
+        <li>Algorithms</li>
+        <li>Kafka</li>
+        <li>WebSockets</li>
+        <li>Grpc</li>
+        <li>Redis</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Education",
+    id: "education",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>
+          B.Tech (Heritage Institute Of Technology , Kolkata (2020))
+          <br />
+          C.G.P.A (9.23)
+        </li>
+      </ul>
+    ),
+  },
+  {
+    title: "Experience",
+    id: "experience",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Software Development Engineer-2 - Upstox(Rksv) (Current)</li>
+        <li>Software Development Engineer-1 - Upstox(Rksv)</li>
+        <li>System Engineer - TCS</li>
+      </ul>
+    ),
+  },
+];
 const AboutSection = () => {
   const [tabState, setTabState] = useState("skills");
   const [isPending, startTransition] = useTransition();
@@ -53,7 +57,7 @@ const AboutSection = () => {
   };
   return (
     <section className="text-white pt-24" id="aboutMe">
-      <div className="md:grid  md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16" >
+      <div className="md:grid  md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16">
         <Image src="/images/about-image.png" width={500} height={500} />
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
@@ -90,7 +94,7 @@ const AboutSection = () => {
             </TabButton>
           </div>
           <div className="mt-8">
-                {TAB_DATA.find((t) => t.id===tabState).content}
+            {TAB_DATA.find((t) => t.id === tabState).content}
           </div>
         </div>
       </div>
